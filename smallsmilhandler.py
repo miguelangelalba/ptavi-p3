0dc83ea9fb269028a38986afec38901a226ddc4e
miguelangel3 <miguelangel.abb@gmail.com>
2017-10-12 14:32:35 +0200

    Comienzo una nueva posible estructura, para buscar los atributos. Me doy cuenta de que la librería que usamos mete los atributos en un diccionario

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -9,7 +9,7 @@ region = ["id", "top", "bottom", "left", "right"]
 img = ["src", "region", "begin", "dur"]
 audio = ["src", "begin", "dur"]
 textstream = ["src", "region"]
-encontradas = []
+
 
 etiquetas = {
     "root-layout": rootlayout,
@@ -21,18 +21,28 @@ etiquetas = {
 
 class SmallSMILHandler(ContentHandler):
     """docstring for SmallSMILHandler."""
-    def __init__(self, arg):
 
+    def __init__(self, arg):
+        self.encontradas = []
         self.arg = arg
     def get_tags(self):
         pass
 
     def startElement(self,name, attrs):
 
-        for clave in etiquetas
-            for atributo in clave.atributos
+        if name not in etiquetas.keys():
+            return
 
-                if name == atributo
+        for atributo in etiquetas[name]:
+
+            if attrs == atributo:
+
+
+
+        for clave in etiquetas:
+            for atributo in clave.atributos:
+
+                if name == atributo:
                     self.calificacion = attrs.get('calificacion', "")
                     print (self.calificacion)
 
