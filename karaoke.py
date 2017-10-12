95e0cf388e8aae6b79b8b1e204887e65678c8eaa
miguelangel3 <miguelangel.abb@gmail.com>
2017-10-12 20:48:01 +0200

    Modifico la manera de ver los datos, antes no lo hacía como se pedía

--- karaoke.py
+++ karaoke.py
@@ -8,8 +8,11 @@ import smallsmilhandler
 
 def mostrar_valores(lista):
     for elemento in lista:
+        atr_a_imprimir = ""
         for atributo in elemento["atributos"]:
-            print(elemento["etiqueta"] + "\t" + atributo + "\t" + elemento["atributos"][atributo])
+            atr_a_imprimir = atr_a_imprimir + "\t" + atributo + "\t" + elemento["atributos"][atributo]
+
+        print(elemento["etiqueta"] + atr_a_imprimir)
 
 if __name__ == '__main__':
 
