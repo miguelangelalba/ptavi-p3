db6ccd9cecb44ef208cf9fba195dfeb3dfc2be71
miguelangel3 <miguelangel.abb@gmail.com>
2017-10-13 18:57:00 +0200

    Arreglo problema que he dectectado al mostrar los datos en la shell y empiezo la parte de json

--- karaoke.py
+++ karaoke.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import json
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 import smallsmilhandler
@@ -13,18 +14,24 @@ def mostrar_valores(lista):
             if elemento["atributos"][atributo] != "":
                 atr_a_imprimir = atr_a_imprimir + "\t" + atributo + "=" + \
                 elemento["atributos"][atributo]
-        if elemento["atributos"][atributo] != "":
-            print(elemento["etiqueta"] + atr_a_imprimir)
+        #if elemento["atributos"][atributo] != "":
+        print(elemento["etiqueta"] + atr_a_imprimir)
+
+def to_json(fichero,lista):
+    nombre_fich = fichero.split(".")[0] + ".json"
+    with open(nombre_fich,"w") as fich_json:
+        json.dumps(lista)
+    #print("json",data_string)
 
 if __name__ == '__main__':
 
     parser = make_parser()
     cHandler = smallsmilhandler.SmallSMILHandler()
     parser.setContentHandler(cHandler)
-    print(len(sys.argv))
     if len(sys.argv) != 2:
         sys.exit("Usage:python3 karaoke.py file.smil.")
-
-    parser.parse(open(sys.argv[1]))
-    lista =cHandler.get_tags()
+    fichero = sys.argv[1]
+    parser.parse(open(fichero))
+    lista = cHandler.get_tags()
     mostrar_valores(lista)
+    #to_json(fichero,lista)
